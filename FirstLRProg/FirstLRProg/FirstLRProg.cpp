c731207cbf9f10a589f57f98eadcbf544d26f905
Nikita Kolesnikov <nikitapka@gmail.com>
2020-03-04 20:05:29 +0500

    123

--- FirstLRProg/FirstLRProg/FirstLRProg.cpp
+++ FirstLRProg/FirstLRProg/FirstLRProg.cpp
@@ -2,12 +2,16 @@
 #include <cmath>
 
 using namespace std;
-
+// Ax2+Bx+c=0
 int main() {
 	double A, B, C;
 	cin >> A >> B >> C;
+	
 	double D = B * B - 4 * A * C;
-	if (D == 0) {
+	if (A == 0 && !B==0 ) { 
+		cout << (-C/B) << endl;
+	}
+	else if (D == 0 && (!B==0 && !A==0)) {
 		cout << -B / (2 * A) << endl;
 	}
 	else if (D > 0) {
