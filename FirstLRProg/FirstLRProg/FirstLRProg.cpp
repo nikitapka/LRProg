114a92c4fe6a96894336f78c13414a13d4c731f1
nikitapka <nikitapka@users.noreply.github.com>
2020-03-07 17:59:14 +0500

    8гп87

--- FirstLRProg/FirstLRProg/FirstLRProg.cpp
+++ FirstLRProg/FirstLRProg/FirstLRProg.cpp
@@ -1,34 +1,80 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 
-vector<int> Reversed(const vector<int>& v) {
-    vector<int> numbers = v;
-    if (!numbers.empty())
-    {
-        size_t i = numbers.size();
-        do {
-            --i;
-            numbers.push_back(numbers[i]);
-            numbers.erase(numbers.begin() + i);
-        } while (i > 0);
-    }
-    return numbers;
+void NextMounth(int& CurrentMounth) {
+	if (CurrentMounth < 11) {
+		CurrentMounth++;
+	}
+	else
+	{
+		CurrentMounth = 0;
+	}
+}
+
+void RefreshMounth(vector<string>& v, const int& CurrentMounth) {
+	vector<int> Mounths = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	int NumberDays = Mounths[CurrentMounth];
+	//if (v.empty())
+	//{
+	//	v.resize(NumberDays);
+	//}
+	/*else
+	{*/
+		while (v.size() < NumberDays)
+		{
+			v.resize(NumberDays);
+		}
+
+		while (v.size() > NumberDays)
+		{
+		//	v[v.size - 2] += " ";
+			v[v.size() - 2] += v[v.size() - 1];
+			v.pop_back();
+		}
+	//}
+}
+
+void ChangeMounth(vector<string>& v, int& CurrentMounth) {
+	NextMounth(CurrentMounth);
+	RefreshMounth(v, CurrentMounth);
+}
+
+void AddRecord(vector<string>& v, int NumberDay) {
+	string Record;
+	cin << Record;
+	v[NumberDay - 1] += (Record + " ");
 }
 
 int main() {
-    int NumberOperation;
-    string Operation;
-    int argument;
-    cin >> NumberOperation;
-    vector<string> AvailableOperations = { "WORRY" }
-    for (int i; i < NumberOperation; i++)
-    {
-        cin >> Operation;
-        
-    }
-    return 0;
+	int NumberOperations;
+	cin >> NumberOperations >> endl;
+	string Operation;
+	int CurrentMounth = 0;
+	int NumberDay;
+	vector<string> Mounth();
+	RefreshMounth(Mounth, CurrentMounth);
+	for  (int i=0;  i< NumberOperations; i++)
+	{
+		cin >> Operation;
+		if (Operation="ADD")
+		{
+			cin >> NumberDay;
+			AddRecord(Mounth, NumberDay);
+		} 
+		else if (Operation = "NEXT") {
+			ChangeMounth(Mounth, CurrentMounth);
+		}
+		else if (Operation = "DUMP")
+		{
+			cout
+		}
+
+
+	}
+	return 0;
 }
